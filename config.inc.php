9e06eeb8e8ce87bdfa3574d51eb84ffd5cf197a7
zhangchunhui <boycot2017@163.com>
2024-12-24 17:49:02 +0800

    111

--- config.inc.php
+++ config.inc.php
@@ -20,15 +20,14 @@ require_once __TYPECHO_ROOT_DIR__ . '/var/Typecho/Common.php';
 // config db
 $db = new \Typecho\Db('Pdo_Mysql', 'typecho_');
 $db->addServer(array (
-  'host' => 'sql.freedb.tech',
+  'host' => 'mysql.sqlpub.com',
   'port' => 3306,
-  'user' => 'freedb_boycot_blog',
-  'password' => 'EPGDg%FYXZcP9s#',
-  'charset' => 'utf8mb4',
-  'database' => 'freedb_boycot_blog',
-//   1134573460@qq.com
+  'user' => 'boycot_blog',
+  'password' => 'KdIEhCKaGFDBn4LH',
+  'charset' => 'utf8mb4_unicode_ci',
+  'database' => 'boycot_blog',
 //   'engine' => 'MyISAM',
   // 'sslCa' => '',
-  // 'sslVerify' => true,
+  'sslVerify' => true,
 ), \Typecho\Db::READ | \Typecho\Db::WRITE);
 \Typecho\Db::set($db);
