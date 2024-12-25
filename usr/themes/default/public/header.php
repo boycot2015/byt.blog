dbc7fdde76003c1484a4eeeb331a2c4f8948a23e
zhangchunhui <boycot2017@163.com>
2024-12-25 15:45:45 +0800

    兼容

--- usr/themes/default/public/header.php
+++ usr/themes/default/public/header.php
@@ -84,11 +84,11 @@
         <?php $this->widget('Widget_Contents_Page_List')->to($pages); ?>
         <?php if (count($pages->stack) <= $this->options->JNavMaxNum) : ?>
           <?php foreach ($pages->stack as $item) : ?>
-            <a class="item <?php echo $this->is('page', $item['slug']) ? 'active' : '' ?>" href="<?php echo $item['url'] ?>" title="<?php echo $item['title'] ?>"><?php echo $item['title'] ?></a>
+            <a class="item <?php echo $this->is('page', $item['slug']) ? 'active' : '' ?>" href="<?php echo $item['permalink']??('/' + ($item['slug']||'')) ?>" title="<?php echo $item['title'] ?>"><?php echo $item['title'] ?></a>
           <?php endforeach; ?>
         <?php else : ?>
           <?php foreach (array_slice($pages->stack, 0, $this->options->JNavMaxNum) as $item) : ?>
-            <a class="item <?php echo $this->is('page', $item['slug']) ? 'active' : '' ?>" href="<?php echo $item['permalink'] ?>" title="<?php echo $item['title'] ?>"><?php echo $item['title'] ?></a>
+            <a class="item <?php echo $this->is('page', $item['slug']) ? 'active' : '' ?>" href="<?php echo $item['permalink']??('/' + ($item['slug']||'')) ?>" title="<?php echo $item['title'] ?>"><?php echo $item['title'] ?></a>
           <?php endforeach; ?>
           <div class="joe_dropdown" trigger="hover" placement="60px" style="margin-right: 15px;">
             <div class="joe_dropdown__link">
